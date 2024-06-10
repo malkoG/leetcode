8a49f607f91011b0e28aaf819944f2a733d0f58d
malkoG <rijgndqw012@gmail.com>
2024-06-10 13:11:02 +0900

    Revenge weekly401D

--- weekly401/D_test.dart
+++ weekly401/D_test.dart
@@ -1,17 +1,41 @@
-import 'package:test/test.dart';
-import 'dart:collection';
 import 'dart:math';
 
+import 'package:test/test.dart';
+import 'dart:collection';
+
 class Solution {
-  int numberOfPairs(List<int> nums1, List<int> nums2, int k) {
-    return 1;
+  List<bool> candidates = List<bool>.generate(101010, (index) => false);
+
+  int maxTotalReward(List<int> rewardValues) {
+    SplayTreeSet<int> sortedRewardValues = SplayTreeSet<int>.from(rewardValues);
+
+    for (int rewardValue in rewardValues) {
+      candidates[rewardValue] = true;
+      for (int i = 0; i < rewardValue; i++) {
+        if (candidates[i] && i < rewardValue) {
+          candidates[i + rewardValue] = true;
+        }
+      }
+    }
+
+    int answer = 0;
+    for (int i = 0; i < 101010; i++) {
+      if (candidates[i]) {
+        answer = max(answer, i);
+      }
+    }
+
+    return answer;
   }
 }
 
 void main() {
-  test("", () {
-    expect(1, equals(1));
+  test("Testing maxTotalReward", () {
+    expect(Solution().maxTotalReward([1, 1, 3, 3]), equals(4));
+    expect(Solution().maxTotalReward([1, 6, 4, 3, 2]), equals(11));
+    expect(Solution().maxTotalReward([1, 5, 4]), equals(9));
+    expect(Solution().maxTotalReward([3, 4, 17, 11]), equals(32));
+    expect(Solution().maxTotalReward([19, 1, 14, 13]), equals(34));
+    expect(Solution().maxTotalReward([5, 8, 9, 12]), equals(21));
   });
-
-  test("", () {});
 }
